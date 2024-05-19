95d3f693b7f5ebeb6b0e24ca84f01254933f68d1
JulioHernandez <ju010970@gmail.com>
2024-05-19 10:06:59 -0600

    se agrega jwt y variables de entorno

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,16 @@
-import {ApplicationConfig} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 import {ServerErrorInterceptor} from "./interceptor/server-error.interceptor";
+import {environment} from "../environments/environment.development";
+import {JwtModule} from "@auth0/angular-jwt";
+
+export function tokenGetter(){
+  return sessionStorage.getItem(environment.TOKEN_NAME);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +18,15 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     // provideHttpClient(), // config clasica
     provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(
+      JwtModule.forRoot({
+        config:{
+          tokenGetter: tokenGetter,
+          allowedDomains:["localhost:8080"],
+          disallowedRoutes:["http://localhost:8080/login/forget"]
+        },
+      })
+    ),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ServerErrorInterceptor,
