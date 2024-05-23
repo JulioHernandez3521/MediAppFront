5086344b10b49b0002864884dec041a4219e2193
JulioHernandez <ju010970@gmail.com>
2024-05-22 22:48:45 -0600

    se poner ruta 403 y el guard a nivel de rura padre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import {Routes} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {LayaotComponent} from "./pages/layaot/layaot.component";
-import {certGuard} from "./guard/cer.guard";
 import {Not404Component} from "./pages/not404/not404.component";
+import {Not403Component} from "./pages/not403/not403.component";
+import {certGuard} from "./guard/cer.guard";
 
 export const routes: Routes = [
   {path:"",component:LoginComponent},
@@ -10,7 +11,10 @@ export const routes: Routes = [
   {path:"pages",
     component:LayaotComponent,
     loadChildren: ()=> import('./pages/pages.route').then((m)=>m.routes),
-    canActivate:[certGuard]
+    // canActivate:[certGuard]
+  },
+  {
+    path: 'not-403' , component:Not403Component
   },
   {path:'not-404', component:Not404Component},
   {path:'**', redirectTo:'not-404'}
